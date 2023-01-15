65c72140be6f1af3d240b6debfecd9d765a7911d
서민지 <squarearth1@gmail.com>
2023-01-15 17:18:56 +0900

    MemberGradeScheduler 작성

---
+++ src/main/java/shop/yesaladin/batch/scheduler/SchedulerConfig.java
@@ -0,0 +1,32 @@
+package shop.yesaladin.batch.scheduler;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+/**
+ * Spring Scheduler 설정 입니다.
+ *
+ * @author 서민지
+ * @since 1.0
+ */
+@Configuration
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    private static final int SCHEDULE_THREAD_SIZE = 5;
+
+    public ThreadPoolTaskScheduler threadPoolTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(SCHEDULE_THREAD_SIZE);
+        threadPoolTaskScheduler.setThreadNamePrefix("SCHEDULER-");
+        threadPoolTaskScheduler.initialize();
+
+        return threadPoolTaskScheduler;
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setTaskScheduler(threadPoolTaskScheduler());
+    }
+}
