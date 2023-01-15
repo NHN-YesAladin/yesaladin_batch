a82f341901b4e0b965dde8ce4b6826b277149f7b
서민지 <squarearth1@gmail.com>
2023-01-15 19:40:00 +0900

    JobParametersValidator 작성

--- src/main/java/shop/yesaladin/batch/job/BatchJobConfig.java
+++ src/main/java/shop/yesaladin/batch/job/BatchJobConfig.java
@@ -2,9 +2,11 @@ package shop.yesaladin.batch.job;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersValidator;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +26,20 @@ public class BatchJobConfig {
     private final Step updateMemberPointStep;
 
     /**
+     * updateMemberJob 의 필수 파라미터를 지정하는 validator 입니다.
+     *
+     * @return 조회 기간 설정에 필요한 필수 파라미터를 가진 validator
+     */
+    @Bean
+    public JobParametersValidator validator() {
+        DefaultJobParametersValidator validator = new DefaultJobParametersValidator();
+
+        validator.setRequiredKeys(new String[] {"startDate", "endDate"});
+
+        return validator;
+    }
+
+    /**
      * 지난달 주문 금액에 따라 회원의 등급을 수정하는 step 과 등급별 포인트를 지급하는 step 을 수행하는 Job 입니다.
      *
      * @return 2개의 step 을 실행하는 updateMemberJob
@@ -34,6 +50,7 @@ public class BatchJobConfig {
                 .get("updateMemberJob")
                 .start(updateMemberGradeStep)
                 .next(updateMemberPointStep)
+                .validator(validator())
                 .build();
     }
 }
