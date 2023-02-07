200ba79b9ae92fac6de39794b1fa9f07f6d757fa
서민지 <squarearth1@gmail.com>
2023-02-07 22:49:55 +0900

    [Feat]: logncrash 설정

--- src/main/java/shop/yesaladin/batch/scheduler/BirthdayCouponScheduler.java
+++ src/main/java/shop/yesaladin/batch/scheduler/BirthdayCouponScheduler.java
@@ -28,7 +28,7 @@ public class BirthdayCouponScheduler {
 
     private final Job giveBirthdayCouponJob;
     private final JobLauncher jobLauncher;
-    private static final String ONE_AM_EVERY_DAY = "0 0 1 * * *";
+    private static final String ONE_AM_EVERY_DAY = "0/10 * * * * *";
     private static final int LATER_DAYS = 7;
 
     /**
